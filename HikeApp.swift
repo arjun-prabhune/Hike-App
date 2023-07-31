b6d9394af62d219654c4188d9419676a93fabd99
arjun-prabhune <98632891+arjun-prabhune@users.noreply.github.com>
2023-07-31 17:54:42 -0500

    Add files via upload

---
+++ HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Arjun Prabhune on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
